76b2b88b34f2ffd7c6c1fa8d936a8835870175cb
Azzou <60717896+AzzouDuGhetto@users.noreply.github.com>
2020-09-09 20:00:05 +0200

    bump to 1.1

--- Prefs/CoeusRootListController.m
+++ Prefs/CoeusRootListController.m
@@ -16,7 +16,7 @@
 	self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,0,10,10)];
 	self.titleLabel.font = [UIFont boldSystemFontOfSize:17];
 	self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
-	self.titleLabel.text = @"1.0";
+	self.titleLabel.text = @"1.1";
 	self.titleLabel.textColor = [UIColor colorWithRed:0.96 green:0.77 blue:0.75 alpha:1.0];
 	self.titleLabel.textAlignment = NSTextAlignmentCenter;
 
